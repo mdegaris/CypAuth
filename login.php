669f2f011eb6991892ad9c3ca548ab10d79f6ef7
mdegaris <mdegaris@gmail.com>
2023-05-15 15:43:35 +0100

    daily

--- login.php
+++ login.php
@@ -3,8 +3,9 @@
 $LOGIN_ATTEMPT = false;
 $LOGIN_SUCCESS = false;
 
-$COOKIE_LIFE = time() + 60 * 60 * 24 * 365 * 2;  // Two years
+$COOKIE_LIFE = time() + 60 * 60 * 24 * 365;  // One year
 $DOMAIN = 'cyprotex.com';
+
 
 function build_auth_cookie($login_name, $login_location)
 {
