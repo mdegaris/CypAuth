4a4e353c3fa8c99667d13408b31fc3f1fbbfa81e
mdegaris <mdegaris@gmail.com>
2023-05-26 22:32:20 +0100

    delete_cookie.php added

--- public/delete_cookie.php
+++ public/delete_cookie.php
@@ -5,3 +5,22 @@ if (isset($_COOKIE[$cookieName])) {
     unset($_COOKIE[$cookieName]);
     setcookie($cookieName, '', time() - 3600);
 }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible"
+          content="IE=edge" />
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1.0" />
+</head>
+
+<body>
+    COOKIE DELETE!!!
+</body>
+
+</html>
